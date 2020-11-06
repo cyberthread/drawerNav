b9443a546b9b1344cb3f16298c536a7a55efb00a
Rebecca Mushtare <rebecca.mushtare@oswego.edu>
2020-11-05 21:30:49 -0500

    pull out js into separate doc

---
+++ drawer.js
@@ -0,0 +1,8 @@
+     /* place the navigation on the page */
+	 function openNav() {
+		document.getElementById("theDrawer").style.right = "0";
+	}
+	/* place the navigation off the page */
+	function closeNav() {
+		document.getElementById("theDrawer").style.right = "-250px";
+	}
